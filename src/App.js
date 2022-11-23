5675398e241a1f30308619fde8f0bf441c344204
feri <ferialfahri@gmail.com>
2022-11-23 19:40:57 +0000

    Setup project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import * as React from "react";
 import { ethers } from "ethers";
-import './App.css';
+import ParticlesBg from "particles-bg";
+import "./App.css";
 
 export default function App() {
+  const wave = () => {};
 
-  const wave = () => {
-    
-  }
-  
   return (
     <div className="mainContainer">
-
+      <ParticlesBg color="#81D99C" type="cobweb" bg={true} />
       <div className="dataContainer">
         <div className="header">
-        👋 Hey there!
+          <span role="img" aria-label="Love">
+            👋
+          </span>{" "}
+          Hey there!
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+          <p>I am delta and I am learning to build an Ethereum dApp!</p>
+          <p>Connect your Ethereum wallet and wave at me!</p>
         </div>
 
-        <button className="waveButton" onClick={wave}>
+        <button className="f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-dark-green" onClick={wave}>
           Wave at Me
         </button>
       </div>
